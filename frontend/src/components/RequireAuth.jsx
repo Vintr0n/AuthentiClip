14ab49700aae176376d688be72cab9434c974828
Vintr0n <stuartvinton@gmail.com>
2025-07-25 22:42:13 +0100

    Changes session

--- frontend/src/components/RequireAuth.jsx
+++ frontend/src/components/RequireAuth.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from './AuthContext';
 
 export default function RequireAuth({ children }) {
   const { user, loading } = useAuth();
