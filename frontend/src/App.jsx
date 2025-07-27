c987c893e0e37703b972b9453275b47408e51d34
Vintr0n <stuartvinton@gmail.com>
2025-07-27 16:40:25 +0100

    FE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,17 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import RequireAuth from './components/RequireAuth';
+import Header from './components/Header';
+
+import About from './pages/About';
+import FAQ from './pages/FAQ';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import UploadVideo from './pages/UploadVideo';
+import Verify from './pages/Verify';
+
 export default function App() {
   return (
-    <div className="min-h-screen text-white flex flex-col overflow-hidden bg-[#0e131f]">
+    <div className="min-h-screen h-screen bg-gradient-to-br from-sky-300 to-blue-600 text-white flex flex-col overflow-hidden">
       <Header />
       <main className="flex-grow flex justify-center items-center px-4">
         <Routes>
