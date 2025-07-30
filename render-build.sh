54a60df7c079eb1faa2bfc8ee709019324d4e877
Vintr0n <stuartvinton@gmail.com>
2025-07-30 23:31:01 +0100

    Cleaned up ffmpeg conversion logic and dependencies

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Extract the FFmpeg binary from the bundled tar.xz file
-mkdir -p bin
-tar -xf ffmpeg-release-amd64-static.tar.xz --strip-components=1 -C bin
+#mkdir -p bin
+#tar -xf ffmpeg-release-amd64-static.tar.xz --strip-components=1 -C bin
 
 # Make ffmpeg binary executable
-chmod +x bin/ffmpeg
+#chmod +x bin/ffmpeg
 
 # Install Python dependencies
 pip install -r requirements.txt
