72fdf764d6276bf75c9aff53d1e874d864700da1
Vintr0n <stuartvinton@gmail.com>
2025-08-01 18:00:14 +0100

    Mobile use version 3

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -50,23 +50,24 @@ export default function Header() {
         {/* Dropdown Menu */}
         {menuOpen && (
           <div className="mt-3 bg-gray-800 rounded-lg p-4 space-y-2 shadow-lg absolute left-4 right-4 top-full">
-            <nav className="flex flex-col gap-2">
-              {!user && (
-                <>
-                  <NavLink to="/login" className={navLinkClass}>Login</NavLink>
-                  <NavLink to="/signup" className={navLinkClass}>Signup</NavLink>
-                </>
-              )}
-              <NavLink to="/about" className={navLinkClass}>About</NavLink>
-              <NavLink to="/POC" className={navLinkClass}>POC</NavLink>
-              {user && (
-                <>
-                  <NavLink to="/upload" className={navLinkClass}>Upload</NavLink>
-                  <NavLink to="/verify" className={navLinkClass}>Verify</NavLink>
-                  <NavLink to="/feedback" className={navLinkClass}>Feedback</NavLink>
-                </>
-              )}
-            </nav>
+<nav className="flex flex-col gap-2">
+  {!user && (
+    <>
+      <NavLink to="/login" className={navLinkClass} onClick={() => setMenuOpen(false)}>Login</NavLink>
+      <NavLink to="/signup" className={navLinkClass} onClick={() => setMenuOpen(false)}>Signup</NavLink>
+    </>
+  )}
+  <NavLink to="/about" className={navLinkClass} onClick={() => setMenuOpen(false)}>About</NavLink>
+  <NavLink to="/POC" className={navLinkClass} onClick={() => setMenuOpen(false)}>POC</NavLink>
+  {user && (
+    <>
+      <NavLink to="/upload" className={navLinkClass} onClick={() => setMenuOpen(false)}>Upload</NavLink>
+      <NavLink to="/verify" className={navLinkClass} onClick={() => setMenuOpen(false)}>Verify</NavLink>
+      <NavLink to="/feedback" className={navLinkClass} onClick={() => setMenuOpen(false)}>Feedback</NavLink>
+    </>
+  )}
+</nav>
+
 
             {user && (
               <div className="text-xs text-gray-300 text-center">Hello, {user}</div>
