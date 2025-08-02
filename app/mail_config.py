9c5fbe3ebdade0c9f59a294cc589474f397a60c8
Vintr0n <stuartvinton@gmail.com>
2025-08-02 22:56:57 +0100

    Email verification backend fix

--- app/mail_config.py
+++ app/mail_config.py
@@ -5,9 +5,9 @@ conf = ConnectionConfig(
     MAIL_PASSWORD="youwillneverguesswhativebeendoing",
     MAIL_FROM="noreply@clipcert.com",
     MAIL_PORT=587,
-    MAIL_SERVER="smtp.ionos.co.uk",  # or smtp.ionos.com for US servers
-    MAIL_TLS=True,
-    MAIL_SSL=False,
+    MAIL_SERVER="smtp.ionos.co.uk",
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True
 )
