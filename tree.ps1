4f673250480957bb25a152ebd8bcdf8ac7a6cb45
Vintr0n <stuartvinton@gmail.com>
2025-08-17 23:50:09 +0100

    Update to make verify and upload pages visible

--- tree.ps1
+++
@@ -1,6 +0,0 @@
-﻿Get-ChildItem -Recurse -Force | Where-Object {
-    $_.FullName -notmatch '\\node_modules\\' -and
-    $_.FullName -notmatch '\\.git\\'
-} | ForEach-Object {
-    $_.FullName.Replace((Get-Location).Path, '.') 
-} > tree.txt
