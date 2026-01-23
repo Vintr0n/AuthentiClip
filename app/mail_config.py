4e80f172ee660455b2382fc58a30acfdb2de5af9
Vintr0n <49721167+Vintr0n@users.noreply.github.com>
2026-01-23 00:06:31 +0000

    Update mail_config.py

--- app/mail_config.py
+++ app/mail_config.py
@@ -1,11 +1,11 @@
 from fastapi_mail import ConnectionConfig
 
 conf = ConnectionConfig(
-    MAIL_USERNAME="noreply@clipcert.com",
-    MAIL_PASSWORD="youwillneverguesswhativebeendoing",
-    MAIL_FROM="noreply@clipcert.com",
+    MAIL_USERNAME="",
+    MAIL_PASSWORD="",
+    MAIL_FROM="",
     MAIL_PORT=587,
-    MAIL_SERVER="smtp.ionos.com",
+    MAIL_SERVER="",
     MAIL_STARTTLS=False,
     MAIL_SSL_TLS=True,
     USE_CREDENTIALS=True,
